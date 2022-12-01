6ec3788b6d36b5b12b117d87489ee2be86c3545b
Christopher Rosenvall <christopher.rosenvall@dataductus.se>
2022-12-01 11:06:33 +0100

    Add project files.

---
+++ AoC2022/main.cs
@@ -0,0 +1,13 @@
+﻿using Applier;
+using System;
+
+namespace AdventOfCode2022
+{
+	class AdventOfCode2022
+	{
+		static public void Main(String[] args)
+		{
+			Day1 day1 = new Day1();
+		}
+	}
+}
---
+++ Day1/Solution.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Day1
+{
+	private string input;
+	public Day1()
+	{
+		input = File.ReadAllText("input.txt");
+	}
+
+	public int Part1()
+	{
+		return AvailableCalories(input).First();
+	}
+
+	public int Part2()
+	{
+		return AvailableCalories(input).Take(3).Sum();
+	}
+
+	private IEnumerable<int> AvailableCalories(string input) =>
+		from section in input.Split("\r\n\r\n")
+		let calorieSum = section.Split("\r\n").Select(int.Parse).Sum()
+		orderby calorieSum descending
+		select calorieSum;
+}
+
+
+class cls
+{
+	static public void Main(String[] args)
+	{
+
+		Day1 day1 = new Day1();
+		Console.WriteLine($"Part 1: {day1.Part1()}");
+		Console.WriteLine($"Part 2: {day1.Part2()}");
+	}
+}
