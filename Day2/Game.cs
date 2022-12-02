21c33b931c47e50bcd57f939d60573ecc4229f85
Christopher Rosenvall <christopher.rosenvall@sogeti.com>
2022-12-02 15:20:00 +0100

    day2

---
+++ Day2/Game.cs
@@ -0,0 +1,48 @@
+﻿public class Game
+{
+	public int Score { get; init; }
+
+	public Game(char opponentPlay, char myMove)
+	{
+		Score = CalculateScore(opponentPlay, myMove);
+	}
+
+	private int CalculateScore(char oppenentPlays, char myMove)
+	{
+		int opp = ToValue(oppenentPlays);
+		int me = ToValue(myMove);
+
+		int turnPoints = CheckWin(opp, me);
+
+		return turnPoints + me;
+	}
+
+	private int CheckWin(int opponentPlays, int myMove) => myMove switch
+	{
+		// Sten
+		1 when opponentPlays == myMove => 3,
+		1 when opponentPlays == 2 => 0,
+		1 when opponentPlays == 3 => 6,
+
+		// Påse
+		2 when opponentPlays == myMove => 3,
+		2 when opponentPlays == 1 => 6,
+		2 when opponentPlays == 3 => 0,
+
+		// Sax
+		3 when opponentPlays == myMove => 3,
+		3 when opponentPlays == 1 => 0,
+		3 when opponentPlays == 2 => 6,
+
+		_ => throw new ArgumentOutOfRangeException(nameof(opponentPlays), $"Not expected char  value: {opponentPlays}"),
+	};
+
+
+	private static int ToValue(char chr) => chr switch
+	{
+		'A' or 'X' => 1,
+		'B' or 'Y' => 2,
+		'C' or 'Z' => 3,
+		_ => throw new ArgumentOutOfRangeException(nameof(chr), $"Not expected char  value: {chr}"),
+	};
+}
---
+++ Day2/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Day2
+{
+	private readonly string input = File.ReadAllText("input.txt");
+
+	public int Part1()
+	{
+		return Parse.Sum(x => x.Score);
+	}
+
+	private IEnumerable<Game> Parse =>
+		from lines in input.Split("\r\n")
+		let games = lines.Split(" ").Select(char.Parse).ToList()
+		let game = new Game(games[0], games[1])
+		select game;
+}
+
+class cls
+{
+	static public void Main(String[] args)
+	{
+
+		Day2 day2 = new Day2();
+		Console.WriteLine($"Part 1: {day2.Part1()}");
+		//Console.WriteLine($"Part 2: {day2.Part2()}");
+	}
+}
